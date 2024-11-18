6b4ebf82f8a6fd3bfd83068821367de661dfa554
abduganii <abduganiabdu2@gmail.com>
2024-11-18 11:57:26 +0500

    ds

--- src/components/read-books.tsx
+++ src/components/read-books.tsx
@@ -1,16 +1,10 @@
 'use client';
 import React from 'react';
-import HTMLFlipBook from 'react-pageflip';
 
 const BookSlider: React.FC = () => {
  
   return (
-    <HTMLFlipBook width={300} height={500}>
-            <div className="demoPage">Page 1</div>
-            <div className="demoPage">Page 2</div>
-            <div className="demoPage">Page 3</div>
-            <div className="demoPage">Page 4</div>
-        </HTMLFlipBook>
+    <>hello</>
   );
 };
 
